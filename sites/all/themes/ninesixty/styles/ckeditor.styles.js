d6a3b66b86dbb78f881e612189d25030201dae0a
Benjamin Christensen <benjamin@cyberhus.dk>
2011-08-23 11:20:48 +0200

    adding icons to ckeditor style dropdown

--- sites/all/themes/ninesixty/styles/ckeditor.styles.js
+++ sites/all/themes/ninesixty/styles/ckeditor.styles.js
@@ -27,8 +27,6 @@ CKEDITOR.addStylesSet( 'drupal',
 	{ name : 'Address'			, element : 'address' },
 	*/
 
-	{ name : 'Blue Title'		, element : 'h3', styles : { 'color' : 'Blue' } },
-	{ name : 'Red Title'		, element : 'h3', styles : { 'color' : 'Red' } },
 
 	/* Inline Styles */
 
@@ -43,48 +41,9 @@ CKEDITOR.addStylesSet( 'drupal',
 	{ name : 'Superscript'		, element : 'sup' },
 	*/
 
-	{ name : 'Marker: Yellow'	, element : 'span', styles : { 'background-color' : 'Yellow' } },
-	{ name : 'Marker: Green'	, element : 'span', styles : { 'background-color' : 'Lime' } },
+	{ name : 'Link: Lydfil' , element : 'span', styles : { 'background-image' : 'url(/sites/all/themes/ninesixty/images/enterung_ikon_lyd.png)' , 'padding-top' : '2px' ,'padding-left' : '25px' ,  'background-repeat' : 'no-repeat' } },
+  { name : 'Link: Dokument' , element : 'span', styles : { 'background-image' : 'url(/sites/all/themes/ninesixty/images/enterung_ikon_doc.png)' , 'padding-top' : '2px' ,'padding-left' : '25px' ,  'background-repeat' : 'no-repeat' } },
+  { name : 'Link: Fotoalbum' , element : 'span', styles : { 'background-image' : 'url(/sites/all/themes/ninesixty/images/enterung_ikon_foto.png)' , 'padding-top' : '2px' ,'padding-left' : '25px' ,  'background-repeat' : 'no-repeat' } },
 
-	{ name : 'Big'				, element : 'big' },
-	{ name : 'Small'			, element : 'small' },
-	{ name : 'Typewriter'		, element : 'tt' },
 
-	{ name : 'Computer Code'	, element : 'code' },
-	{ name : 'Keyboard Phrase'	, element : 'kbd' },
-	{ name : 'Sample Text'		, element : 'samp' },
-	{ name : 'Variable'			, element : 'var' },
-
-	{ name : 'Deleted Text'		, element : 'del' },
-	{ name : 'Inserted Text'	, element : 'ins' },
-
-	{ name : 'Cited Work'		, element : 'cite' },
-	{ name : 'Inline Quotation'	, element : 'q' },
-
-	{ name : 'Language: RTL'	, element : 'span', attributes : { 'dir' : 'rtl' } },
-	{ name : 'Language: LTR'	, element : 'span', attributes : { 'dir' : 'ltr' } },
-
-	/* Object Styles */
-
-	{
-		name : 'Image on Left',
-		element : 'img',
-		attributes :
-		{
-			'style' : 'padding: 5px; margin-right: 5px',
-			'border' : '2',
-			'align' : 'left'
-		}
-	},
-
-	{
-		name : 'Image on Right',
-		element : 'img',
-		attributes :
-		{
-			'style' : 'padding: 5px; margin-left: 5px',
-			'border' : '2',
-			'align' : 'right'
-		}
-	}
 ]);
